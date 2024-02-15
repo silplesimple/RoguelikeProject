7bff904ec123334ecf0026c067f2f3e13144174c
silplesimple <154574815+silplesimple@users.noreply.github.com>
2024-02-15 19:43:27 +0900

    feat.맵 문 기능 구현

--- Assets/SilpleSimple/Scripts/Map/Door.cs
+++ Assets/SilpleSimple/Scripts/Map/Door.cs
@@ -5,44 +5,6 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    private bool monsterPresent = false;
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if(other.CompareTag("Monster"))
-        {
-            monsterPresent = true;
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if(other.CompareTag("Monster"))
-        {
-            monsterPresent = false;
-        }
-    }
-
-    private void Start()
-    {
-        StartCoroutine(CheckMonsterAndOpenDoor());
-    }
-
-    IEnumerator CheckMonsterAndOpenDoor()
-    {
-        while(true)
-        {
-            yield return null;
-            if (!monsterPresent)
-            {
-                OpenDoor();
-                yield break;
-            }
-        }
-    }
-    void OpenDoor()
-    {
-
-    }
+    
 
 }
--- Assets/SilpleSimple/Scripts/Map/Floor.cs
+++ Assets/SilpleSimple/Scripts/Map/Floor.cs
@@ -4,8 +4,63 @@ using UnityEngine;
 
 public class Floor : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    private bool monsterPresent = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.CompareTag("Monster"))
+        {
+            monsterPresent = true;
+            Debug.Log("몬스터 왔다!");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Monster"))
+        {
+            monsterPresent = false;
+            Debug.Log("몬스터 없다!"+monsterPresent);
+        }
+    }
+
+    private void Start()
+    {
+        StartCoroutine(CheckMonsterAndOpenDoor());
+    }
+
+    IEnumerator CheckMonsterAndOpenDoor()
+    {
+        while (true)
+        {
+            yield return null;
+            if (!monsterPresent)
+            {                
+                OpenDoor(false);
+                yield break;
+            }
+            else if (monsterPresent)
+            {
+                OpenDoor(true);
+                yield break;
+            }
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (!monsterPresent)
+            OpenDoor(false);
+        else if(monsterPresent)
+            OpenDoor(true);
+    }
+    private void OpenDoor(bool door){
         
+        Transform doorTransform = transform.Find("Door");
+        if (doorTransform != null)
+        {
+            
+            doorTransform.gameObject.SetActive(door);
+            
+        }
     }
 }
